203f0bf9f53439e2e6c8f5b1d38e3e8301815dfe
Juandiego Patiño Vargas <juanp221@gmail.com>
2021-05-02 19:15:42 -0500

    Ajustes infra

--- infra.sh
+++ infra.sh
@@ -3,4 +3,5 @@ aws s3api create-bucket --bucket $SAM_CODE_BUCKET --region us-east-1
 sam package --template-file sam/sam-template.yaml --output-template-file sam/sam-deploy.yaml --s3-bucket $SAM_CODE_BUCKET
 cd terraform
 terraform init
+terraform destroy -lock=false -auto-approve
 terraform apply -lock=false -auto-approve
